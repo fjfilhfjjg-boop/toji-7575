715904138465829c0c928fb27009448e652da7cd
fjfilhfjjg-boop <fjfilhfjjg@gmail.com>
2026-04-19 19:18:05 +0200

    Add قلوب.js with character questions and responseshsbsb

---
+++ قلوب.js
@@ -0,0 +1,17 @@
+[
+  {
+    "type": "character",
+    "question": "لوفي",
+    "response": "لوفي"
+  },
+  {
+    "type": "character",
+    "question": "ايتاتشي",
+    "response": "ايتاتشي"
+  },
+  {
+    "type": "character",
+    "question": "ليفاي",
+    "response": "ليفاي"
+  }
+]
